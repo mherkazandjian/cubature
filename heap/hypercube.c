86eb9663f81ec385ac3bf5f88f326b98038dba53
Mher Kazandjian <mher.kazandjian@surf.nl>
2024-12-03 23:17:30 +0100

    document the hypercube functions and header and add examples in main

--- heap/hypercube.c
+++ heap/hypercube.c
@@ -1,6 +1,16 @@
 #include <stdlib.h>
 #include "hypercube.h"
 
+/**
+ * @brief Computes the volume of a hypercube.
+ *
+ * This function calculates the volume of a given hypercube by multiplying
+ * the lengths of its sides. Each side length is determined by the data
+ * stored in the hypercube structure.
+ *
+ * @param h Pointer to the hypercube structure.
+ * @return The volume of the hypercube.
+ */
 double compute_vol(const hypercube *h)
 {
      unsigned i;
@@ -10,6 +20,17 @@ double compute_vol(const hypercube *h)
      return vol;
 }
 
+/**
+ * @brief Creates a hypercube structure with specified dimensions, center, and half-widths.
+ *
+ * This function allocates memory for the hypercube data, initializes it with the provided
+ * center and half-width values, and computes the volume of the hypercube.
+ *
+ * @param dim The number of dimensions of the hypercube.
+ * @param center A pointer to an array of doubles representing the center coordinates of the hypercube.
+ * @param halfwidth A pointer to an array of doubles representing the half-widths of the hypercube along each dimension.
+ * @return A hypercube structure initialized with the provided parameters. If memory allocation fails, the volume will be 0.
+ */
 hypercube make_hypercube(unsigned dim, const double *center, const double *halfwidth)
 {
      unsigned i;
@@ -27,6 +48,19 @@ hypercube make_hypercube(unsigned dim, const double *center, const double *halfw
      return h;
 }
 
+/**
+ * @brief Creates a hypercube with specified range.
+ *
+ * This function initializes a hypercube structure with the given dimension,
+ * minimum and maximum bounds. It calculates the center and half-length of
+ * each dimension and stores them in the hypercube's data array. The volume
+ * of the hypercube is also computed and stored.
+ *
+ * @param dim The dimension of the hypercube.
+ * @param xmin Pointer to an array containing the minimum bounds for each dimension.
+ * @param xmax Pointer to an array containing the maximum bounds for each dimension.
+ * @return A hypercube structure initialized with the specified range.
+ */
 hypercube make_hypercube_range(unsigned dim, const double *xmin, const double *xmax)
 {
      hypercube h = make_hypercube(dim, xmin, xmax);
@@ -41,6 +75,64 @@ hypercube make_hypercube_range(unsigned dim, const double *xmin, const double *x
      return h;
 }
 
+/**
+ * @brief Functions for displaying hypercube information
+ *
+ * This file contains functions for printing the content of a hypercube.
+ * The printed content includes:
+ *   - the dimension of the hypercube
+ *   - the data of the hypercube (in each dimension)
+ *       - dim X:
+ *           - the center of the hypercube
+ *           - the half-widths of the hypercube
+ *   - the volume of the hypercube
+ *
+ * Example usage:
+ *
+ * For dim = 1:
+ *   usage
+ *     unsigned dim = 1;
+ *     double xmin[] = {-1.0};
+ *     double xmax[] = {1.0};
+ *     hypercube h = make_hypercube_range(1, xmin, xmax);
+ *     print_hypercube(&h);
+ *   expected output:
+ *     dim = 1
+ *       dim 0: center = 0.000000, half-width = 1.000000
+ *     vol = 2.000000
+ *
+ * For dim = 2:
+ *   usage
+ *     unsigned dim = 2;
+ *     double center[] = {0.0, 0.0};
+ *     double halfwidth[] = {1.0, 1.0};
+ *     hypercube h2 = make_hypercube(2, center, halfwidth);
+ *     print_hypercube(&h2);
+ *     destroy_hypercube(&h2);
+ *   expected output:
+ *     dim = 2
+ *       dim 0: center = 0.000000, half-width = 1.000000
+ *       dim 1: center = 0.000000, half-width = 1.000000
+ *     vol = 4.000000
+ */
+void print_hypercube(const hypercube *h)
+{
+     unsigned i;
+     printf("dim = %u\n", h->dim);
+     for (i = 0; i < h->dim; ++i)
+       printf("  dim %u: center = %f, half-width = %f\n",
+          i, h->data[i], h->data[i + h->dim]);
+     printf("vol = %f\n", h->vol);
+}
+
+/**
+ * @brief Destroys a hypercube object by freeing its allocated data and resetting its dimension.
+ *
+ * This function releases the memory allocated for the data of the given hypercube
+ * and sets its dimension to 0.
+ *
+ * @param h Pointer to the hypercube object to be destroyed.
+ */
 void destroy_hypercube(hypercube *h)
 {
      free(h->data);
--- heap/main.c
+++ heap/main.c
@@ -12,6 +12,54 @@ int main(int argc, char **argv)
     unsigned int fdim = 1;
     regions = heap_alloc(1, fdim);
 
+    /*
+       hypercube example usage
+    */
+    hypercube h;
+
+    // define a hypercube of dimension 1 with a certain range (xmin,xmax)
+    printf("Create a hypercube of dimension 1 with:\n");
+    printf("      xrange (-1,1)\n");
+    unsigned dim1 = 1;
+    double xmin1[] = {-1};
+    double xmax1[] = {1};
+
+    // create the hypercube and print its content
+    h = make_hypercube_range(dim1, xmin1, xmax1);
+    print_hypercube(&h);
+    destroy_hypercube(&h);
+    printf("--------------------\n");
+
+    // define a hypercube of dimension 2 with a certain range (xmin,xmax)
+    // and (ymin,ymax)
+    printf("Create a hypercube of dimension 2 with:\n");
+    printf("      xmin (-1,-6)\n");
+    printf("      xmax (1,-2)\n");
+    unsigned dim2 = 2;
+    double xmin2[] = {-1, -6};
+    double xmax2[] = {1, -2};
+
+    // create the hypercube and print its content
+    h = make_hypercube_range(dim2, xmin2, xmax2);
+    print_hypercube(&h);
+    destroy_hypercube(&h);
+    printf("--------------------\n");
+
+    // define a hypercube of dimension 3 with a certain range (min,max)
+    printf("Create a hypercube of dimension 3 with xrange (-1,1) and yrange (-6,-2)\n");
+    printf("      xmin (-1, -6, 4)\n");
+    printf("      xmax (1 , -2, 12)\n");
+
+    unsigned dim3 = 3;
+    double xmin3[] = {-1, -6, 4};
+    double xmax3[] = {1, -2, 12};
+
+    // create the hypercube and print its content
+    h = make_hypercube_range(dim3, xmin3, xmax3);
+    print_hypercube(&h);
+    destroy_hypercube(&h);
+    printf("--------------------\n");
+
     printf("hello world\n");
     exit(0);
 }
